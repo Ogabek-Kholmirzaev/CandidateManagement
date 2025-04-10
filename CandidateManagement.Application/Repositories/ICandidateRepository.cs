e157e1c4f4b2bc6555344bc13cb7343ee3084db1
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-04-10 21:26:30 +0500

    ICandidateRepository

---
+++ CandidateManagement.Application/Repositories/ICandidateRepository.cs
@@ -0,0 +1,10 @@
+﻿using CandidateManagement.Domain.Entities;
+
+namespace CandidateManagement.Application.Repositories;
+
+public interface ICandidateRepository
+{
+    Task<Candidate?> GetByEmailAsync(string email);
+    Task AddAsync(Candidate candidate);
+    Task UpdateAsync(Candidate candidate);
+}
