bc5b4bc2840122f356cd024bb29a035bcf833b96
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-04-10 21:30:57 +0500

    ICandidateService

---
+++ CandidateManagement.Application/Services/ICandidateService.cs
@@ -0,0 +1,9 @@
+﻿using CandidateManagement.Application.DTOs;
+using CandidateManagement.Domain.Entities;
+
+namespace CandidateManagement.Application.Services;
+
+public interface ICandidateService
+{
+    Task<Candidate> AddOrUpdateAsync(CandidateDto dto);
+}
