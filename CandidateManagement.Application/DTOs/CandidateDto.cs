8c31667dd51f4cf751d230d31bbbc25031ea69d8
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-04-10 20:46:16 +0500

    Candidate entity & dto

---
+++ CandidateManagement.Application/DTOs/CandidateDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CandidateManagement.Application.DTOs;
+
+public class CandidateDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? PhoneNumber { get; set; }
+    public TimeOnly? StartCallTime { get; set; }
+    public TimeOnly? EndCallTime { get; set; }
+    public string? LinkedInProfileUrl { get; set; }
+    public string? GitHubProfileUrl { get; set; }
+    public string Comment { get; set; } = string.Empty;
+}
---
+++ CandidateManagement.Domain/Entities/Candidate.cs
@@ -0,0 +1,39 @@
+﻿namespace CandidateManagement.Domain.Entities;
+
+public class Candidate
+{
+    public int Id { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public string? PhoneNumber { get; private set; }
+    public TimeOnly? StartCallTime { get; private set; }
+    public TimeOnly? EndCallTime { get; private set; }
+    public string? LinkedInProfileUrl { get; private set; }
+    public string? GithubProfileUrl { get; private set; }
+    public string Comment { get; private set; }
+
+    private Candidate() { }
+
+    public Candidate(
+        string firstName,
+        string lastName,
+        string email,
+        string? phoneNumber,
+        TimeOnly? startCallTime,
+        TimeOnly? endCallTime,
+        string? linkedInProfileUrl,
+        string? githubProfileUrl,
+        string comment)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        StartCallTime = startCallTime;
+        EndCallTime = endCallTime;
+        LinkedInProfileUrl = linkedInProfileUrl;
+        GithubProfileUrl = githubProfileUrl;
+        Comment = comment;
+    }
+}
